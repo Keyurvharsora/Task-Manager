ef6220fec11c1bc978f7baebda8dd3f6fba37821
KEYUR <keyurharsora09@gmail.com>
2024-03-09 11:55:01 +0530

    Work-Manager

---
+++ src/app/api/tasks/route.js
@@ -0,0 +1,46 @@
+// /tasks
+
+import { getResponseMessage } from "@/helper/responseMessage";
+import { Task } from "@/models/task";
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export async function GET(request) {
+  try {
+    const tasks = await Task.find();
+    return NextResponse.json(tasks);
+  } catch (error) {
+    console.log(error);
+    return "Error in gettting data !!", 404, false;
+  }
+}
+
+export async function POST(request) {
+  const { title, content, userId, status } = await request.json();
+
+  const authToken = request.cookies.get("authToken")?.value;
+  
+  const data = jwt.verify(authToken, process.env.JWT_KEY);
+  
+  console.log(data._id);
+
+  try {
+    const task = new Task({
+      title,
+      content,
+      userId: data._id,
+      status,
+    });
+
+    const createdTask = await task.save();
+    return NextResponse.json(createdTask, {
+      status: 201,
+    });
+  } catch (error) {
+    console.log(error);
+    console.log("my message");
+    return getResponseMessage("Failed to create Task !! ", 500, false);
+  }
+}
+
+//
